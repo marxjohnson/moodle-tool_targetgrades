f648ec444f79f971edbf6c4f7d17a0ab21f04632
Mark Johnson <mark.johnson@it.ox.ac.uk>
2013-04-04 15:01:47 +0100

    Created web page branch

--- module.js
+++
@@ -1,6 +0,0 @@
-M.tool_targetgrades = {
-    init_datalist: function(Y) {
-        Y.all('.ie input[list]').hide().set('disabled', 'disabled');
-        Y.all('.safari input[list]').hide().set('disabled', 'disabled');
-    }
-}
